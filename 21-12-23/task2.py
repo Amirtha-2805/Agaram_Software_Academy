8542a12b4d606c0279cabd8ebc156c7a3df01326
Amirtha-2805 <amirthanatarajan14@gmail.com>
2023-12-22 06:50:39 -0800

    file added

--- 21-12-23/task2.py
+++ 21-12-23/task2.py
@@ -1,56 +1,87 @@
 my_resume={
     "name":"Amirtha.N",
-    "education_qualification":[{
-        "standard":"10th",
-        "subject":"Maths Biology",
-        "marks-scored":485,
-        "school-name":"St.Joseph's convent Higher Secondary School"
-    },
-    {
-        "standard":"12th",
-        "subject":"Maths Biology",
-        "marks-scored":1004,
-        "school-name":"St.Joseph's convent Higher Secondary School"
-    },
-    {
+    "education_qualification":[
+        {
+        "course":"10th",
+        "major":"Maths Biology",
+        "percentage":97,
+        "institute_name":"St.Joseph's convent Higher Secondary School"
+          },
+         {
+        "course":"HSC",
+        "major":"Maths Biology",
+        "percentage":90,
+        "institute_name":"St.Joseph's convent Higher Secondary School"
+         },
+         {
 
-        "degree":"B.sc",
-        "department":"Mathamatics",
-        "percentage":"85%",
-        "college-name":"Government Arts and Science College,Konam"
-    }
+        "course":"B.sc",
+        "major":"Mathematics",
+        "percentage":85,
+        "institute_name":"Government Arts and Science College,Konam"
+        },
+        {
+        "course":"M.Sc",
+        "major":"Mathematics",
+        "percentage":90,
+        "institute_name":"Vivekananda College"
+        }
         
         
         ],
-    "experience":[
-        {
+    "experience":
+             [
+             {
         "company_name":"Guidehouse",
         "years_of_experience":3
-    },
-{
-    "company_name":"Hcl",
-    "years-of-experience":1
+            },
+           {
+             "company_name":"Hcl",
+             "years_of_experience":1
 
-}
+           }
     
-    ],
-    "projects":[{
-        "title":"The Riccati Equation",
+          ],
+    "projects":[
+        {
+        "title":"The Riccati's Equation",
         "major":"Mathematics",
         "subject":"Differential Equations"
-    },
-    {
+        },
+        {
 
         "title":"Rings",
         "major":"Mathematics",
         "subject":"Algebra"
-    }
+        }
     ],
-    "certificates":{
+    "certificates":
         
-        "type-writing":["English lower","English higher"]
+            [
+                
+                { 
+                 "course_name":"English Typewriting",
+                 "level":"Junior",
+                 "year_of_completion":2013
 
-    },
+            
+                 },
+                 {
+                  "course_name":"English Typewriting",
+                  "level":"Higher",
+                  "year_of_completion":2015
+
+                 },
+                 {
+                     "course_name":"Computer on office Automation",
+                     "level":"",
+                     "year_of_completion":2023
+                     
+
+                 }
+          ],
+
+    
     "email":"amirthanatarajan14@gmail.com",
     "mobile":"+916385811613",
     "skills":["English Typing","Team work","Patience"],
@@ -58,18 +89,17 @@ my_resume={
     "personal_information":{
     
         "date_of_birth":"14-05-2001",
-        "father name":"Natarajan.S",
-        "mother name":"Jayasree.G",
+        "father_name":"Natarajan.S",
+        "mother_name":"Jayasree.G",
         "address":"15/A, Ammasimadam Street,Meenakshipuram,Nagercoil",
         "pin_code":"629 001",
         "hobbies":["Singing","Cooking","Drawing","Listening to music"],
-        "languages-known":["Tamil","English"]
+        "languages_known":["Tamil","English"]
 
     },
     "declaration":"I declared that the informations given above are true",
     "date":"21-12-23"
 
 }
-print(my_resume["education_qualification"][1])
-print(my_resume["projects"][0])
-print(my_resume["personal_information"]["languages-known"][1])
+
+print(my_resume["certificates"][1]["course_name"])
---
+++ 22-12-23/task1.py
@@ -0,0 +1,561 @@
+application_form={
+                  "degree":"M.Sc",
+                  "branch":"Mathematics",
+                  "name_of_the_applicant":"Amirtha.N",
+                  "nationality":"Indian",
+                  "nativity":"Tamil Nadu",
+                  "sex":"Female",
+               "date_of_birth":{
+                   "date":14,
+                   "month":5,
+                   "year":2001
+               },
+               "age":"22 years",
+               "category":{"community":"BC",  
+                           "caste":"Viswakarma",
+                           "religion":"Hindu",
+                           "blood_group":"A1 positive"
+                          },
+                "family_members":[ 
+                                 {"name":"Natarajan.S",
+                                  "relation":"Father",
+                                  "occupation":"Coolie"
+                               },
+                               {
+                                   "name":"Jayasree.G",
+                                   "relation":"Mother",
+                                   "occupation":"House wife"
+                               },
+                                {
+                                    "name":"Aiswarya.N",
+                                    "relation":"Sister",
+                                    "occupation":""
+
+                                }
+                ],
+                "total_annual_income":96000,
+                "name_of_guardian":"",
+                "relationship_of_guardian":"",
+                
+                "place_of_birth":{
+                    "town":"Kollam",
+                    "district":"Kollam",
+                    "state":"Kerala",
+                    "country":"India"
+                },
+                "address":[ 
+                           {
+                                     "address_type":"Permanent Address",
+                                     "door_no":"15/A",
+                                     "street":"Ammasimadam Street",
+                                     "city":"Nagercoil",
+                                     "town":"Meenakshipuram",
+                                     "district":"Kanyakumari",
+                                     "pin_code":"629 001",
+                                     "Mobile":6385811613,
+                                     "fax_no":"",
+                                     "email":"amirthanatarajan14@gmail.com"
+                                               },
+
+                        {
+
+                              "address_type":"Communication address",
+                               "door_no":"15/A",
+                               "street":"Ammasimadam Street",
+                               "city":"Nagercoil",
+                               "town":"Meenakshipuram",
+                               "district":"Kanyakumari",
+                               "pin_code":"629 001",
+                               "Mobile":6385811613,
+                               "fax_no":"",
+                               "email":"amirthanatarajan14@gmail.com"      
+
+                        },
+                        {
+                             "address_type":"local guardian address",
+                               "door_no":"15/A",
+                               "street":"Ammasimadam Street",
+                               "city":"Nagercoil",
+                               "town":"Meenakshipuram",
+                               "district":"Kanyakumari",
+                               "pin_code":"629 001",
+                               "Mobile":8056921176,
+                               "fax_no":"",
+                               "email":"natarajan18@gmail.com"          
+                        }
+                ],
+                "extra_curricular_activities":{
+
+                                        "sports":"",
+                                        "Games":"",
+                                        "N.C.C":"",
+                                        "N.S.S":"",
+                                        "others":"Yes"
+
+                },
+                "details_of_examination_passed":[
+                                              {
+                                                  "level":"HSC",
+                                                  "name_of-the_examination":"Annual Public",
+                                                  "reg.no":"20183031517104",
+                                                  "name_of_institution":"St.Joseph's convent Higher Sec.School",
+                                                  "month_year":"April 2018",
+                                                  "subjects":["Tamil","English","Maths","Physics","Chemistry","Biology"],
+                                                  "percentage":87,
+                                            
+                                              },
+
+                                            {
+                                                "level":"Diplomo",
+                                                  "name_of-the_examination":"",
+                                                  "reg.no":"",
+                                                  "name_of_institution":"",
+                                                  "month_year":"",
+                                                  "subjects":"",
+                                                  "percentage":87
+                                                                                
+                                            },
+                                            {
+                                                "level":"B.SC",
+                                                  "name_of-the_examination":"Final Semester",
+                                                  "reg.no":"20213031517104",
+                                                  "name_of_institution":"Government Arts and Science College",
+                                                  "month_year":"February 2021",
+                                                  "subjects":["Tamil","English","Number Theory","Complex Analysis","Real Analysis","Algebra"],
+                                                  "percentage":89
+                                            
+
+                                            }
+
+                                           ],
+    "mother_tongue":"Tamil",
+    "hs_academic_stream":[
+                        {
+                            "subject":"Physics",
+                            "marks_obtained":175,
+                            "maximum_marks":200,
+                            "percentage":88
+
+                        },
+
+                        {
+                            "subject":"Chemistry",
+                            "marks_obtained":160,
+                            "maximum_marks":200,
+                            "percentage":80
+
+                        },
+
+                        {
+
+                            "subject":"Mathematics",
+                            "marks_obtained":185,
+                            "maximum_marks":200,
+                            "percentage":93
+                        }
+
+                       ],
+
+    "pre_final_semester":
+    [
+        {"subject":"Algebra1",
+         "grade_obtained":"A+",
+         "maximum_grade":"O+",
+         "percentage":85
+
+
+    },
+
+    {
+        "subject":"Analysis",
+         "grade_obtained":"A",
+         "maximum_grade":"O+",
+         "percentage":80
+
+    },
+    {
+        "subject":"Complex Analysis",
+         "grade_obtained":"B+",
+         "maximum_grade":"O+",
+         "percentage":75
+    },
+    {
+        "subject":"Number Theory",
+         "grade_obtained":"A",
+         "maximum_grade":"O+",
+         "percentage":87
+    },
+    {
+        "subject":"Real Analysis",
+         "grade_obtained":"A",
+         "maximum_grade":"O+",
+         "percentage":80
+    },
+    {
+        "subject":"Number Theory",
+         "grade_obtained":"A",
+         "maximum_grade":"O+",
+         "percentage":80
+
+    }
+
+    ],
+    
+    "final_semester":[
+        
+        {
+
+        "subject":"Graph Theory",
+         "grade_obtained":"A+",
+         "maximum_grade":"O+",
+         "percentage":85
+    },
+
+    {
+        "subject":"Algebra2",
+         "grade_obtained":"O",
+         "maximum_grade":"O+",
+         "percentage":90
+
+    },
+    {
+        "subject":"Functional Analysis",
+         "grade_obtained":"A+",
+         "maximum_grade":"O+",
+         "percentage":85
+
+    },
+    {
+        "subject":"Real Analysis2",
+         "grade_obtained":"B",
+         "maximum_grade":"O+",
+         "percentage":70
+
+    },
+    {
+        "subject":"Advanced Algebra",
+         "grade_obtained":"A",
+         "maximum_grade":"O+",
+         "percentage":80
+    },
+    {
+        "subject":"Advanced Calculus",
+         "grade_obtained":"A+",
+         "maximum_grade":"O+",
+         "percentage":87
+    }
+    ],
+    "marks_out_of_200":{"percentage_of_pre_final_semester":81,
+                        "percentage_of_final_semester":83,
+                        "total_marks":164
+
+    },
+    
+       "b.sc_marks_particulars": 
+       {
+           "semI":[{"subject":"Algebra",
+                    "grade_obtained":"A+",
+                    "max_grade":"O+"
+           },
+           {"subject":"RealAnalysis",
+                    "grade_obtained":"A+",
+                    "max_grade":"O+"
+           },
+           {"subject":"Complex Analysis",
+                    "grade_obtained":"O+",
+                    "max_grade":"O+"
+           }
+           ],
+           "semII":[
+               
+               {"subject":"Algebra2",
+                    "grade_obtained":"A+",
+                    "max_grade":"O+"
+           },
+            {"subject":"Analysis",
+                    "grade_obtained":"A",
+                    "max_grade":"O+"
+           },
+           {"subject":"Functional",
+                    "grade_obtained":"A+",
+                    "max_grade":"O+"
+           },
+
+
+           ],
+           "semIII":[
+               {"subject":"Algebra3",
+                    "grade_obtained":"A+",
+                    "max_grade":"O+"
+           },
+           {"subject":"Analysis2",
+                    "grade_obtained":"A+",
+                    "max_grade":"O+"
+           },
+           {"subject":"Graph Theory",
+                    "grade_obtained":"A+",
+                    "max_grade":"O+"
+           },
+                     ],
+             "semIV":[
+                 {"subject":"Ring Theory",
+                    "grade_obtained":"O+",
+                    "max_grade":"O+"
+                },
+                {"subject":"Algebraic Number Theory",
+                    "grade_obtained":"B+",
+                    "max_grade":"O+"
+           },
+           {"subject":"Advanced Calculus",
+                    "grade_obtained":"O",
+                    "max_grade":"O+"
+           },
+             ],
+             "semV":[
+                 
+
+                    {"subject":"Functional Analysis",
+                    "grade_obtained":"B+",
+                    "max_grade":"O+"
+           },
+           {"subject":"Advanced Algebra1",
+                    "grade_obtained":"A",
+                    "max_grade":"O+"
+           },
+           {"subject":"Complex Analysis",
+                    "grade_obtained":"B+",
+                    "max_grade":"O+"
+           },
+
+             ],
+             "semVI":[
+                 {"subject":"Advanced Differential Equations",
+                    "grade_obtained":"B",
+                    "max_grade":"O+"
+           },
+           {"subject":"Boolean Algebra",
+                    "grade_obtained":"B+",
+                    "max_grade":"O+"
+           },
+           {"subject":"Statistics",
+                    "grade_obtained":"A",
+                    "max_grade":"O+"
+           },
+
+
+             ]       
+
+
+       },     
+    "office_verification":[
+                {
+                  "particulars":"Completion of Application",
+                  "verified_by":"",
+                  "remarks":""
+                    
+                },
+                {
+                  "particulars":"Qualifying Examination Marks",
+                  "verified_by":"",
+                  "remarks":""
+                    
+                },
+                {
+                  "particulars":"List of document enclosed",
+                  "verified_by":"",
+                  "remarks":""
+                    
+                },
+                {
+                  "particulars":"Declaration of Student and parent",
+                  "verified_by":"",
+                  "remarks":""
+                    
+                }
+    ],
+    "office_use":[
+                 {
+                     "particulars":"Remarks from administrative Officer",
+                     "remarks":""
+
+                 },
+                  {
+                     "particulars":"Remarks from Cashier",
+                     "remarks":""
+
+                 },
+                  {
+                     "particulars":"Remarks from principal",
+                     "remarks":""
+
+                 },
+                  {
+                     "particulars":"Remarks from Admission-In-Change",
+                     "remarks":""
+
+                 }
+    ],
+       "records_enclosed":[
+                     {"document":"SSLC mark sheet",
+                      "attested_copy":"",
+                      "original":"",
+                      "remarks":""
+                      
+                     },
+                     {"document":"HSC/Diplomo/B.E Mark Sheet",
+                      "attested_copy":"",
+                      "original":"",
+                      "remarks":""
+                      
+                     },
+                     {"document":"Diplomo/B.E Provisional Certificate",
+                      "attested_copy":"",
+                      "original":"",
+                      "remarks":""
+                      
+                     },
+                     {"document":"Transfer Certificate",
+                      "attested_copy":"",
+                      "original":"",
+                      "remarks":""
+                      
+                     },
+                     {"document":"Conduct Certificate",
+                      "attested_copy":"",
+                      "original":"",
+                      "remarks":""
+                      
+                     },
+                     {"document":"Allotment Order",
+                      "attested_copy":"",
+                      "original":"",
+                      "remarks":""
+                      
+                     },
+                     {"document":"First Graduate Certificate Issued by the Revenue Authority as per norms",
+                      "attested_copy":"",
+                      "original":"",
+                      "remarks":""
+                      
+                     },
+                     {"document":"Income Certificate",
+                      "attested_copy":"",
+                      "original":"",
+                      "remarks":""
+                      
+                     },
+                     {"document":"Equivalency Certificate",
+                      "attested_copy":"",
+                      "original":"",
+                      "remarks":""
+                      
+                     },
+                     {"document":"4 copies of recent colour passport size photographs",
+                      "attested_copy":"",
+                      "original":"",
+                      "remarks":""
+                      
+                     },
+                     {"document":"others",
+                      "attested_copy":"",
+                      "original":"",
+                      "remarks":""
+                      
+                     }
+       ],
+       "list_of_documents":[
+           
+                            {
+
+                             "document_record":"NRI Status Certificate of the financial supporter issued by the"
+                                                "Indian Embassy of the respective country under seal",
+                              "attested_copy":"",
+                              "original":"",
+                              "remarks":""                  
+                             
+                            },
+                             {
+
+                             "document_record":"Certificate of Relationship between the NRI financial supporter and the candidate issued by the competent authority.Valid Indian Passport of the NRI Financial supporter",
+            
+                              "attested_copy":"",
+                              "original":"",
+                              "remarks":""                  
+                             
+
+                            },
+                             {
+
+                             "document_record":"NRE(Non Resident External)Bank Account Pass Book of the financial supporter",
+                                                
+                              "attested_copy":"",
+                              "original":"",
+                              "remarks":""                  
+                             
+
+                            },
+                             {
+
+                             "document_record":"Evidence of payment of Development charge of US $1000/- to the colleg by the NRI financial supporter(one time payment at the time of admission only)",
+                              "attested_copy":"",
+                              "original":"",
+                              "remarks":""                  
+                             
+
+                            }
+       ],
+       "note":"Triplicate attested copies of above mentioned original certificates should be submitted along with Application"
+
+        }
+print(application_form["family_members"][1])
+print(application_form["hs_academic_stream"][1])
+
+
+
+
+
+
+
+                                                        
+
+
+
+
+
+               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
