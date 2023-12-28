f93bf1d0c8229960efbd6326c071bcc5f4134cfa
Amirtha-2805 <amirthanatarajan14@gmail.com>
2023-12-27 23:04:36 -0800

    tasks completed

--- 27-12-23/task17.py
+++ 27-12-23/task17.py
@@ -1,4 +1,14 @@
-a=int(input("Enter:"))
-b=int(input("Enter:"))
-c=int(input("Enter:"))
+
+a=int(input("Enter a coeff:"))
+b=int(input("Enter a coeff:"))
+c=int(input("Enter a coeff:"))
+x=(-b+(b**2-4*a*c)**0.5)/2*a
+y=(-b-(b**2-4*a*c)**0.5)/2*a
+d=b**2-4*a*c
+if d==0:
+    print("The roots are real and equal:",x,"and",y)
+elif d>0:
+    print("The roots are real and distinct:",x,"and",y)    
+elif d<0:
+    print("The roots are imaginary:",x,"and",y)   
 
--- 27-12-23/taskk9.py
+++ 27-12-23/taskk9.py
@@ -2,7 +2,7 @@ x=input("Enter your input:")
 k="!","@","#","$","%","^","&","*","(",")"
 if ("a"<=x<="z") or ("A"<=x<="Z"):
     print("It is a alphabet")
-elif (x=="!") or(x=="@")or (x=="#") or (x=="$") or (x=="%") or (x=="^") or (x=="&") or (x=="*") or (x=="(") or (x==")") :
+elif (x=="!") or(x=="@")or (x=="#") or (x=="$") or (x=="%") or (x=="^") or (x=="&") or (x=="*") or (x=="(") or (x==")") or (x=="-") or (x=="_") or (x=="=") or (x=="+") or (x=="[") or (x=="]") or (x=="{") or (x=="}") or (x==":") or (x==";") or (x==",") or (x=="<") or (x==".") or (x==">") or (x=="/") or (x=="?") :
     print("It is a special character")
 else:
     print("It is a digit")    
