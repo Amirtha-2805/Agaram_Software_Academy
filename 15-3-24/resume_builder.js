de6c07cfbb233a9e3576a28738999a4fcc7b7fa3
Amirtha-2805 <amirthanatarajan14@gmail.com>
2024-03-20 16:10:10 -0700

    Data added,deleted,edited,readed and viewed in html using fire base data base

--- 15-3-24/resume_builder.js
+++ 15-3-24/resume_builder.js
@@ -190,10 +190,10 @@ function listed_resumes(){
     let renderHTML=''
 
     for(let each in resume_parsed){
-      renderHTML=renderHTML+`<li><a href="view.html?index=${each}"> ${resume_parsed[each].title}</a><div id="btncontainer"><button type="button" id="deletebtn" onclick=delete_data('${each}')>delete</button><a href="edit.html?index=${each}"><button type="button" id="editbtn"> edit</button></a></div></li>`
+      renderHTML=renderHTML+`<li><a href="view.html?index=${each}">${resume_parsed[each].title}</a><div id="btncontainer"><button type="button" id="deletebtn" onclick=delete_data('${each}')>delete</button><a href="edit.html?index=${each}"><button type="button" id="editbtn"> edit</button></a></div></li>`
       document.getElementById("list").innerHTML=renderHTML
-       
 
+      
     //   console.log(renderHTML)
         // list.innerHTML=renderHTML
     }
