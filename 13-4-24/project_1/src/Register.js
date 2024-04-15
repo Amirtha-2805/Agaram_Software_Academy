302f3275b54d91c69b27f67fbf0eaee4ccba64f8
Amirtha-2805 <amirthanatarajan14@gmail.com>
2024-04-14 19:10:04 -0700

    react login validation done

--- 13-4-24/project_1/src/Register.js
+++ 13-4-24/project_1/src/Register.js
@@ -1,61 +1,62 @@
 import { useState } from "react";
-import Register_again from "./Login";
 
 export default function Register(){
-  // let email_id={}
-        const[name,setName]=useState("")
-        const[email,setEmail]=useState("")
-        const[password,setPassword]=useState("")
-        const[user,setUsers]=useState([])
-        const[isLogged,setStateLogged]=useState(false)        
-        const[checkEmail,setCheckEmail]=useState("")
-        const[checkPassword,setCheckPassword]=useState("")
-        const[details,setDetails]=useState("")
+   const[isLogged,setStateLogged]=useState(false)
+   const[name,setName]=useState("")
+   const[email,setEmail]=useState("")
+   const[password,setPassword]=useState("")   
+   const[userDetails,setUserDetails]=useState([]) 
+   const[checkEmail,setCheckEmail]=useState("")
+   const[checkPassword,setCheckPassword]=useState("")
+   const[setEmailPwd,setStateEmailpwd]=useState("")
 
-    const Reg_details=()=>{
-        const regUsers={
+
+   const Register_data=()=>{
+        let User_data={
             name,
             email,
             password
         }
-        setUsers([...user,regUsers])
-    }
-    let loginValidation=()=>{
-        for(let each of user){
-         if((each.email==checkEmail)&&(each.password==checkPassword)){
-           setDetails(each.email)
-            setStateLogged("true")            
-         }
+        setUserDetails([...userDetails,User_data])
+   }
+
+   const LoginValidation=()=>{
+    for(let each of userDetails){
+        if((each.email==checkEmail)&&(each.password==checkPassword)){
+            setStateEmailpwd(each.email)
+            setStateLogged("true")
         }
-        console.log(details)        
     }
-
+   }
     return(
-        <>         
-        <div>
-            {isLogged==true ?<h1>Login page</h1>:null}            
+        <>
+            {isLogged==false ? <h1>Register Page</h1>:null}
             <br/>
-            {isLogged==false?<h2>Register Page</h2>:null}
-            {isLogged==false? <input type="text" placeholder="Enter your name" onKeyUp={(e)=>setName(e.target.value)}/>:null}
+            {isLogged==false ? <input type="text" placeholder="Enter your name" onKeyUp={(e)=>setName(e.target.value)}/>:null}
             <br/>
-            {isLogged==false?<input type="text" placeholder="Enter your email" onKeyUp={(e)=>setEmail(e.target.value)}/>:null}
+            {isLogged==false ? <input type="email" placeholder="Enter your email" onKeyUp={(e)=>setEmail(e.target.value)}/>:null}
             <br/>
-            {isLogged==true?<input type="text" placeholder="Enter your email" onKeyUp={(e)=>setCheckEmail(e.target.value)}/>:null}
+            {isLogged==false ? <input type="password" placeholder="Enter your password" onKeyUp={(e)=>setPassword(e.target.value)}/>:null}
             <br/>
-            {isLogged==false?<input type="text" placeholder="Enter your password" onKeyUp={(e)=>setPassword(e.target.value)}/>:null}
-            <br/>
-            {isLogged==true?<input type="text" placeholder="Enter your password" onKeyUp={(e)=>setCheckPassword(e.target.value)}/>:null}
-            {isLogged==false? <button onClick={Reg_details}>Register</button>:null}            
+            {isLogged==false ? <button onClick={()=>Register_data()}>Register</button>:null}
             {isLogged==false ?<button onClick={()=>setStateLogged(true)}>LoginPage</button>:null}
-            {isLogged==true?<h5>{JSON.stringify(user)}</h5>:null} 
-            {isLogged==false?<h5>{JSON.stringify(user)}</h5>:null} 
-            {isLogged==true ?<button onClick={()=>loginValidation()}>Login</button>:null}
-            {isLogged==true? <button onClick={()=>setStateLogged(false)}>Register_page</button>:null}
-            {isLogged=="true"?<h1>Welcome {details}</h1>:null}
-                   
-        </div>        
-        </>
+            {isLogged==false ?<h5>{JSON.stringify(userDetails)}</h5>:null}
+            {isLogged==true ? <h1>LoginPage</h1>:null}
+            {isLogged==true ? <input type="email" placeholder="Enter your email" onKeyUp={(e)=>setCheckEmail(e.target.value)}/>:null}
+            <br/>
+            {isLogged==true ? <input type="password" placeholder="Enter your password" onKeyUp={(e)=>setCheckPassword(e.target.value)}/>:null}
+            <br/>
+            {isLogged==true? <button onClick={()=>LoginValidation()}>Login</button>:null}
+            {isLogged==true? <button onClick={()=>setStateLogged(false)}>Register Page</button>:null}
+            {isLogged==true?<h5>{JSON.stringify(userDetails)}</h5>:null}
+            {isLogged=="true" ?<h1>Welcome!...{setEmailPwd}</h1>:null}
 
+
+
+
+
+        </>       
+        
     )
     
 
